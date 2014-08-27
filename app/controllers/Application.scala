c96b635452341788b1929b1c093662fa1e2927ff
Johannes Unterstein <j.unterstein@micromata.de>
2014-08-27 08:39:29 +0200

    Step 5.2: Adapted rest of ui

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,12 +2,18 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import helper.ElasticSearchSearchHelper
 
 object Application extends Controller {
 
   def index = Action {
     implicit request =>
       Ok(views.html.index())
+  }
+
+  def search(query: String) = Action {
+    implicit request =>
+      Ok(views.html.index(ElasticSearchSearchHelper.search(query), query))
   }
 
 }
